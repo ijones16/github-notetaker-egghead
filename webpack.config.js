f776ac418192d3a59b025d5ad796c96da4d02097
ijones16 <jones58ian@gmail.com>
2015-11-06 11:03:03 -0800

    added webpack devserver and readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+var buildPath = require('path').join(__dirname, 'public');
 module.exports = {
   entry: "./app/components/Main.js",
   output: {
     filename: "public/bundle.js"
+  },
+  devServer: {
+    inline: true,
+    progress: true,
+
+    // parse host and port from env so this is easy
+    // to customize
+    host: process.env.HOST,
+    port: process.env.PORT
   },
   module: {
     loaders: [
